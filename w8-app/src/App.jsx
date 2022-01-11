2645529aabf2eff604d377364d75976a4590cc15
Mausp7 <archie.tombacz@gmail.com>
2022-01-11 15:15:05 +0100

    Wednesday useState practice

--- w8-app/src/App.jsx
+++ w8-app/src/App.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 const App = () => {
-  
+
+let [isLandingPage, setterF]  = useState("");
+
   return (
     <div>
       <h1>Hello Word!</h1>
-      
+      <button onClick={() => setterF("Landing")}>Landing Page</button>
+      <button onClick={() => setterF("Home")}>Home Page</button>
+      <button onClick={() => setterF("About")}>About Page</button>
+      <hr />
+      {isLandingPage === "Landing" && <div>Landing Page</div>}
+      {isLandingPage === "Home" && <div>Home Page</div>}
+      {isLandingPage === "About" && <div>About Page</div>}      
     </div>
   );
 }
